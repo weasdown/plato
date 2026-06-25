a973b339727c34394948f532a3916a045e3157b7
weasdown <34220924+weasdown@users.noreply.github.com>
2026-06-25 11:03:03 +0100

    feat: define DataFrame datatypes and add dates parsing

--- ingest_data.py
+++ ingest_data.py
@@ -1,14 +1,36 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 data: Path = Path('./data')
 satcat_csv: Path = data / 'satcat.csv'
 satcat_xlsx: Path = data / 'satcat.xlsx'
 
+# Which datatype appears in each column of the file. Dates (LAUNCH_DATE, DECAY_DATE) are handled by the parse_dates and
+# date_format arguments in pd.read_csv()/pd.read_excel().
+datatypes = {'OBJECT_NAME': str,
+             'OBJECT_ID': str,
+             'NORAD_CAT_ID': np.uint16,
+             'OBJECT_TYPE': str,
+             'OPS_STATUS_CODE': str,
+             'OWNER': str,
+             'LAUNCH_DATE': str,
+             'LAUNCH_SITE': str,
+             'DECAY_DATE': str,
+             'PERIOD': float,
+             'INCLINATION': float,
+             'APOGEE': 'Int16',
+             'PERIGEE': 'Int16',
+             'RCS': float,
+             'DATA_STATUS_CODE': str,
+             'ORBIT_CENTER': str,
+             'ORBIT_TYPE': str,
+             }
+
 
 def load_satcat(file: Path) -> pd.DataFrame:
-    df: pd.DataFrame = pd.read_csv(file)
+    df: pd.DataFrame = pd.read_csv(file, dtype=datatypes, parse_dates=[6, 8], date_format='%d/%m/%Y')
     print(f'Loaded DataFrame from {file}\n')
     return df
 
